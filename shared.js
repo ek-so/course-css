ea55a276b11f13598c360ea4daa0c2b46c86f240
ek-so <eksomail@gmail.com>
2025-04-22 09:09:37 +0200

    Section 9. First part

---
+++ shared.js
@@ -0,0 +1,5 @@
+var backdrop = document.querySelector('.backdrop');
+var selectPlanButton = document.querySelectorAll('');
+// console.log(backdrop);
+// console.dir(backdrop);
+backdrop.style.display = 'block';
