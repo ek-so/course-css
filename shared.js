c830281f556c4f2c982cd967f84978d4b7c65cb5
ek-so <eksomail@gmail.com>
2025-04-23 08:09:26 +0200

    Section 9. Assignment

--- shared.js
+++ shared.js
@@ -1,5 +1,21 @@
 var backdrop = document.querySelector('.backdrop');
-var selectPlanButton = document.querySelectorAll('');
+var modal = document.querySelector('.modal');
+var selectPlanButton = document.querySelectorAll('.plan button');
+var closeModal = document.querySelector('.modal__action--negative');
+
 // console.log(backdrop);
 // console.dir(backdrop);
-backdrop.style.display = 'block';
+
+for (var i = 0; i < selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';  
+    });
+}
+
+console.dir(closeModal);
+
+closeModal.addEventListener('click', function() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';  
+});
